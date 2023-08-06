a6f275b36ac9224c92b5278148711f014f8a8b76
tz7 <domain.rlstr@tutanota.com>
2023-08-06 16:41:34 +0200

    Add project files.

---
+++ AiProjects/Controller/TestController.cs
@@ -0,0 +1,32 @@
+﻿using AiProjects.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AiProjects.Controller
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TestController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public TestController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("test-connection")]
+        public IActionResult TestConnection()
+        {
+            try
+            {
+                _context.Database.CanConnect();
+                return Ok("Connected to the database!");
+            }
+            catch
+            {
+                return BadRequest("Failed to connect to the database!");
+            }
+        }
+    }
+}
---
+++ AiProjects/Pages/Index.cshtml.cs
@@ -0,0 +1,53 @@
+﻿using AiProjects.Data;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace AiProjects.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ApplicationDbContext _context;
+
+        public string ConnectionStatus { get; private set; }
+        public List<string> Tables { get; set; } = new List<string>();
+
+        public IndexModel(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public void OnGet()
+        {
+            if (_context.Database.CanConnect())
+            {
+                ConnectionStatus = "Connected to the database!";
+                try
+                {
+                    var connection = _context.Database.GetDbConnection();
+                    connection.Open();
+                    using (var command = connection.CreateCommand())
+                    {
+                        command.CommandText = "SELECT id, test FROM public.test";
+                        using (var reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                var id = reader.GetInt32(0); // Assuming id is an integer
+                                var test = reader.GetString(1); // Assuming test is a string
+                                Tables.Add($"ID: {id}, Test: {test}");
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    ConnectionStatus += " But an exception occurred while fetching tables: " + ex.Message;
+                }
+            }
+            else
+            {
+                ConnectionStatus = "Failed to connect to the database!";
+            }
+        }
+    }
+}
